7d1689d5d00affd63e8747cc83955d3fcb34c5c7
Ilnar <ilnarshan@gmail.com>
2021-01-28 19:34:32 +0400

    Правки финал 7

--- source/js/script.js
+++ source/js/script.js
@@ -18,18 +18,18 @@ noJs.classList.remove("menu-box_no-js");
 
 // tariff-rates popup
 
-// var tariffOn = document.querySelector(".rate-tariff__company-show");
-// var tariffPopup = document.querySelector(".business-rates");
-// var tariffOff = tariffPopup.querySelector(".business-rates__close");
+var tariffOn = document.querySelector(".rate-tariff__company-show");
+var tariffPopup = document.querySelector(".business-rates");
+var tariffOff = tariffPopup.querySelector(".business-rates__close");
 
-// tariffOn.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   tariffPopup.classList.add("business-rates_open");
-// });
+tariffOn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  tariffPopup.classList.add("business-rates_open");
+});
 
-// tariffOff.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   tariffPopup.classList.remove("business-rates_open");
-// });
+tariffOff.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  tariffPopup.classList.remove("business-rates_open");
+});
 
 //mobile menu no js
