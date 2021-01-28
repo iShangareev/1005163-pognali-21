422d74e8c528cbcb6898f55bedc0aa27d1179748
Ilnar <ilnarshan@gmail.com>
2021-01-28 17:52:58 +0400

    Правки финал 6

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,3 @@
-// tariff-rates popup
-
-var tariffOn = document.querySelector(".rate-tariff__company-show");
-var tariffPopup = document.querySelector(".business-rates");
-var tariffOff = tariffPopup.querySelector(".business-rates__close");
-
-tariffOn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  tariffPopup.classList.add("business-rates_open");
-});
-
-tariffOff.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  tariffPopup.classList.remove("business-rates_open");
-});
-
-//mobile menu no js
-
-var noJs = document.querySelector(".menu-box_no-js");
-noJs.classList.remove("menu-box_no-js");
-
 // mobile-menu
 
 var menuOn = document.querySelector(".navbar__menu-button");
@@ -33,3 +12,24 @@ menuOff.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.remove("menu-box_open");
 });
+
+var noJs = document.querySelector(".menu-box_no-js");
+noJs.classList.remove("menu-box_no-js");
+
+// tariff-rates popup
+
+// var tariffOn = document.querySelector(".rate-tariff__company-show");
+// var tariffPopup = document.querySelector(".business-rates");
+// var tariffOff = tariffPopup.querySelector(".business-rates__close");
+
+// tariffOn.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   tariffPopup.classList.add("business-rates_open");
+// });
+
+// tariffOff.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   tariffPopup.classList.remove("business-rates_open");
+// });
+
+//mobile menu no js
